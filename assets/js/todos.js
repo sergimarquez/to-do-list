6fbc0e9ecc23e9b90330cfffb93d6bad55a2354a
Sergi Marquez <sertrack8@gmail.com>
2017-11-14 09:59:32 +0100

    Delete Todos Button

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,14 @@
 // Check Off Specific Todos By Clicking
+
 $("li").click(function(){
 	$(this).toggleClass("completed");
+});
+
+//Click X and delete Todo
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
