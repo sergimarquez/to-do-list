9a728a9e0fc18bd2213c04929361450549f43403
Sergi Marquez <sertrack8@gmail.com>
2017-11-14 11:01:40 +0100

    Add new todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,29 @@
 // Check Off Specific Todos By Clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click X and delete Todo
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
+// Add new Todos
+$("input[type='text']").keypress(function(event){
+	//Grab text when enter is pressed
+	if(event.which === 13){
+		var newEntry = $(this).val();
+		$(this).val("");
+	//Add it to the list
+		$("ul").append("<li><span>X</span> " + newEntry + "</li>");
+	}
+});
+
+	
+
+	
